0da7041cc2ab62829ccb9e31360d5e14d012c71a
Joaquin Bejar <jb@taunais.com>
2024-01-03 14:07:09 +0100

    add OHLC and HeikinAsin candles

--- include/trading_common/ohlc.h
+++ include/trading_common/ohlc.h
@@ -5,4 +5,47 @@
 #ifndef TRADING_COMMON_OHLC_H
 #define TRADING_COMMON_OHLC_H
 
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+namespace trading::common {
+
+    class OHLCException : public std::exception {
+    private:
+        std::string message{};
+    public:
+        explicit OHLCException(std::string message) : message(std::move(message)) {}
+
+        [[nodiscard]] const char *what() const noexcept override {
+            return message.c_str();
+        }
+    };
+
+    struct OHLC {
+        double open{};
+        double high{};
+        double low{};
+        double close{};
+
+        OHLC();
+
+        OHLC(const json &j);
+
+        OHLC(double open, double high, double low, double close);
+
+        [[nodiscard]] json to_json() const;
+
+    };
+
+    struct HeikinAshi : public OHLC {
+
+        HeikinAshi(const OHLC &current, const OHLC &previous);
+
+        HeikinAshi(const OHLC &current);
+
+    };
+
+}
+
 #endif //TRADING_COMMON_OHLC_H
--- src/ohlc.cpp
+++ src/ohlc.cpp
@@ -3,3 +3,47 @@
 //
 
 #include <trading_common/ohlc.h>
+
+namespace trading::common {
+
+    OHLC::OHLC() : open(0), high(0), low(0), close(0) {}
+
+    OHLC::OHLC(const json &j) {
+        try {
+            open = j["open"];
+            high = j["high"];
+            low = j["low"];
+            close = j["close"];
+        } catch (json::exception &e) {
+            throw OHLCException("Error parsing OHLC json: " + std::string(e.what()));
+        }
+    }
+
+    OHLC::OHLC(double open, double high, double low, double close) : open(open), high(high), low(low), close(close) {}
+
+    json OHLC::to_json() const {
+        json j;
+        j["open"] = open;
+        j["high"] = high;
+        j["low"] = low;
+        j["close"] = close;
+        return j;
+    }
+
+
+    HeikinAshi::HeikinAshi(const OHLC &current, const OHLC &previous) {
+        close = (current.open + current.high + current.low + current.close) / 4;
+        open = (previous.open + previous.close) / 2;
+        high = std::max({current.high, open, close});
+        low = std::min({current.low, open, close});
+    }
+
+    HeikinAshi::HeikinAshi(const OHLC &current) {
+        close = (current.open + current.high + current.low + current.close) / 4;
+        // for the first candle, use the traditional open price
+        open = current.open;
+        high = std::max({current.high, open, close});
+        low = std::min({current.low, open, close});
+    }
+
+}
