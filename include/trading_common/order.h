55b6b16c189e5be7771b396955721d08b33af49a
Joaquin Bejar <jb@taunais.com>
2024-01-03 15:40:52 +0100

    add order struct

--- include/trading_common/order.h
+++ include/trading_common/order.h
@@ -4,7 +4,50 @@
 
 #ifndef TRADING_COMMON_ORDER_H
 #define TRADING_COMMON_ORDER_H
+
+#include <cstdlib>
+#include <string>
+#include <common/common.h>
+
 namespace trading::order {
+    class OHLCException : public std::exception {
+    private:
+        std::string message{};
+    public:
+        explicit OHLCException(std::string message);
+
+        [[nodiscard]] const char *what() const noexcept override;
+    };
+
+    enum class Side {
+        NONE = 0,
+        BUY = 1,
+        SELL = 2
+    };
+
+    enum class Type {
+        NONE = 0,
+        MARKET = 1,
+        LIMIT = 2
+    };
+
+
+    struct Order {
+        size_t timestamp = 0;
+        size_t quantity = 0;
+        std::string symbol{};
+        Side side = Side::NONE;
+        size_t filled = 0;
+        double price = 0;
+        double limit_price = 0;
+        std::string id = ::common::key_generator();
+        Type type = Type::NONE;
+        Order() = default;
+
+        Order(json &j);
+
+        json to_json() const;
+    };
 
 }
 #endif //TRADING_COMMON_ORDER_H
--- src/order.cpp
+++ src/order.cpp
@@ -6,4 +6,74 @@
 
 namespace trading::order {
 
+    OHLCException::OHLCException(std::string message) : message(std::move(message)) {}
+
+    [[nodiscard]] const char *OHLCException::what() const noexcept {
+        return message.c_str();
+    }
+
+    Order::Order(json &j) {
+        try {
+            timestamp = j["timestamp"];
+            quantity = j["quantity"];
+            symbol = j["symbol"];
+            filled = j["filled"];
+            price = j["price"];
+            limit_price = j["limit_price"];
+            id = j["id"];
+            std::string side_str = ::common::to_upper(j["side"]);
+            if (side_str == "BUY") {
+                side = Side::BUY;
+            } else if (side_str == "SELL") {
+                side = Side::SELL;
+            } else {
+                side = Side::NONE;
+            }
+            std::string type_str = ::common::to_upper(j["type"]);
+            if (type_str == "MARKET") {
+                type = Type::MARKET;
+            } else if (type_str == "LIMIT") {
+                type = Type::LIMIT;
+            } else {
+                type = Type::NONE;
+            }
+        } catch (json::exception &e) {
+            throw OHLCException("Error parsing OHLC json: " + std::string(e.what()));
+        }
+    }
+
+    json Order::to_json() const {
+        json j;
+        j["timestamp"] = timestamp;
+        j["quantity"] = quantity;
+        j["symbol"] = symbol;
+        j["filled"] = filled;
+        j["price"] = price;
+        j["limit_price"] = limit_price;
+        j["id"] = id;
+        switch (side) {
+            case Side::BUY:
+                j["side"] = "BUY";
+                break;
+            case Side::SELL:
+                j["side"] = "SELL";
+                break;
+            default:
+                j["side"] = "NONE";
+                break;
+        }
+        switch (type) {
+            case Type::MARKET:
+                j["type"] = "MARKET";
+                break;
+            case Type::LIMIT:
+                j["type"] = "LIMIT";
+                break;
+            default:
+                j["type"] = "NONE";
+                break;
+        }
+        return j;
+    }
+
 }
