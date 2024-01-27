ddec3c8e7e398a014132d7b6d44dbcc0ee883f69
Joaquin Bejar <jb@taunais.com>
2024-01-27 20:11:33 +0100

    add PnL

---
+++ include/trading_common/instructions.h
@@ -0,0 +1,94 @@
+//
+// Created by Joaquin Bejar Garcia on 27/1/24.
+//
+
+#ifndef COMMON_INSTRUCTIONS_H
+#define COMMON_INSTRUCTIONS_H
+
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp>
+#include <common/common.h>
+
+namespace common::instructions {
+
+    using json = nlohmann::json;
+    using common::find_key_from_value;
+    typedef std::string type_t;
+    typedef std::string selector_t;
+
+     enum class Type {
+        NONE=0,
+        TICKER=1,
+        OHLC=2,
+        MACD=3,
+        SMA=4,
+        EMA=5
+    };
+
+    const std::map<Type, type_t> TypeNames = {
+            {Type::NONE,    ""},
+            {Type::TICKER,  "ticker"},
+            {Type::OHLC,    "ohlc"},
+            {Type::MACD,    "macd"},
+            {Type::SMA,     "sma"},
+            {Type::EMA,     "ema"}
+    };
+
+    type_t get_type_name(Type type);
+
+    Type get_type_from_string(const type_t &type);
+
+    enum class Selector {
+        NONE=0,
+        ALL=1,
+        ONE=2,
+        SET=3
+    };
+
+    const std::map<Selector, selector_t> SelectorNames = {
+            {Selector::NONE,    ""},
+            {Selector::ALL,     "all"},
+            {Selector::ONE,     "one"},
+            {Selector::SET,     "set"}
+    };
+
+    selector_t get_selector_name(Selector selector);
+
+    Selector get_selector_from_string(const selector_t &selector);
+
+    template<typename T>
+    concept JsonSerializable = requires(T t, const nlohmann::json& j) {
+        { t.to_json() } -> std::same_as<nlohmann::json>;
+        { t.from_json(j) } -> std::same_as<void>;
+    };
+
+    template<typename T>
+    requires JsonSerializable<T>
+    struct Instructions {
+        Type type = Type::NONE;
+        Selector selector = Selector::NONE;
+        std::vector<std::string> tickers;
+        T other;
+
+        [[nodiscard]] json to_json() const {
+            json result;
+            result["type"] = get_type_name(type);
+            result["selector"] = get_selector_name(selector);
+            result["tickers"] = tickers;
+            result["other"] = other.to_json();
+            return result;
+        }
+
+        void from_json(const json &j) {
+            type = get_type_from_string(j["type"].get<std::string>());
+            selector = get_selector_from_string(j["selector"].get<std::string>());
+            tickers = j["tickers"].get<std::vector<std::string>>();
+            if (j.contains("other"))
+                other.from_json(j["other"]);
+        }
+    };
+
+}
+
+#endif //COMMON_INSTRUCTIONS_H
---
+++ test/test_instructions.cpp
@@ -0,0 +1,79 @@
+//
+// Created by Joaquin Bejar Garcia on 27/1/24.
+//
+
+#include <common/instructions.h>
+#include <catch2/catch_test_macros.hpp>
+
+
+using namespace common::instructions;
+
+class simpleJson {
+    public:
+        std::string param1;
+        int param2;
+    json to_json() const {
+        return json{{"param1", param1}, {"param2", param2}};
+    }
+    void from_json(const json &j) {
+        param1 = j.contains("param1") ? j["param1"].get<std::string>() : "";
+        param2 = j.contains("param2") ? j["param2"].get<int>() : 0;
+    }
+};
+
+TEST_CASE("Type enum and functions") {
+    SECTION("get_type_name") {
+        REQUIRE(get_type_name(Type::NONE).empty());
+        REQUIRE(get_type_name(Type::TICKER) == "ticker");
+    }
+
+    SECTION("get_type_from_string") {
+        REQUIRE(get_type_from_string("ticker") == Type::TICKER);
+        REQUIRE(get_type_from_string("invalid") == Type::NONE);
+    }
+}
+
+TEST_CASE("Selector enum and functions") {
+    SECTION("get_selector_name") {
+        REQUIRE(get_selector_name(Selector::NONE).empty());
+        REQUIRE(get_selector_name(Selector::ALL) == "all");
+    }
+
+    SECTION("get_selector_from_string") {
+        REQUIRE(get_selector_from_string("all") == Selector::ALL);
+        REQUIRE(get_selector_from_string("invalid") == Selector::NONE);
+    }
+}
+
+TEST_CASE("Instructions struct") {
+    Instructions<simpleJson> instructions;
+
+    SECTION("to_json and from_json") {
+        instructions.type = Type::EMA;
+        instructions.selector = Selector::ONE;
+        instructions.tickers = {"AAPL", "MSFT"};
+
+        json j = instructions.to_json();
+
+        REQUIRE(j["type"] == "ema");
+        REQUIRE(j["selector"] == "one");
+        REQUIRE(j["tickers"] == json({"AAPL", "MSFT"}));
+
+        json j2 = {{"type", "sma"}, {"selector", "set"}, {"tickers", {"GOOG", "AMZN"}}};
+        instructions.from_json(j2);
+        REQUIRE(instructions.type == Type::SMA);
+        REQUIRE(instructions.selector == Selector::SET);
+        REQUIRE(instructions.tickers == std::vector<std::string>({"GOOG", "AMZN"}));
+
+        json j3 = {{"type", "sma"},
+                   {"selector", "set"},
+                   {"tickers", {"GOOG", "AMZN"}},
+                   {"other", {{"param1", "value1"}, {"param2", 2}}} };
+        instructions.from_json(j3);
+        REQUIRE(instructions.type == Type::SMA);
+        REQUIRE(instructions.selector == Selector::SET);
+        REQUIRE(instructions.tickers == std::vector<std::string>({"GOOG", "AMZN"}));
+        REQUIRE(instructions.other.param1 == "value1");
+        REQUIRE(instructions.other.param2 == 2);
+    }
+}
